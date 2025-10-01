c04e0f89d02c15eb763fde23e04e815bcb67697e
zmapalad <alezandromapalad@gmail.com>
2025-10-01 14:25:35 +0800

    added buttons

--- smartvoyage/app/page.tsx
+++ smartvoyage/app/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Hero from "./_components/Hero";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
