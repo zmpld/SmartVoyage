e69966ad44857026156aa67272b76f94eaa877ce
z <alezandromapalad@gmail.com>
2025-10-08 22:42:08 +0800

    convex db setup

--- smartvoyage/app/layout.tsx
+++ smartvoyage/app/layout.tsx
@@ -3,6 +3,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,7 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={outfit.className}>
-          <Provider> {children} </Provider>
+          <ConvexClientProvider> {children} </ConvexClientProvider>
         </body>
       </html>
     </ClerkProvider>
