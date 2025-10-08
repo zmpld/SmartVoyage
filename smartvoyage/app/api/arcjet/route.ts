76a471c26f9c1d76114efb3b41f81d52360e342e
z <alezandromapalad@gmail.com>
2025-10-08 23:08:04 +0800

    arcjet setup

---
+++ smartvoyage/app/api/arcjet/route.ts
@@ -0,0 +1,31 @@
+import arcjet, { tokenBucket } from "@arcjet/next";
+import { NextResponse } from "next/server";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!, // Get your site key from https://app.arcjet.com
+  rules: [
+    // Create a token bucket rate limit. Other algorithms are supported.
+    tokenBucket({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      characteristics: ["userId"], // track requests by a custom user ID
+      refillRate: 5, // refill 5 tokens per interval
+      interval: 86400, // refill every 10 seconds
+      capacity: 10, // bucket maximum capacity of 10 tokens
+    }),
+  ],
+});
+
+export async function GET(req: Request) {
+  const userId = "user123"; // Replace with your authenticated user ID
+  const decision = await aj.protect(req, { userId, requested: 5 }); // Deduct 5 tokens from the bucket
+  console.log("Arcjet decision", decision);
+
+  if (decision.isDenied()) {
+    return NextResponse.json(
+      { error: "Too Many Requests", reason: decision.reason },
+      { status: 429 },
+    );
+  }
+
+  return NextResponse.json({ message: "Hello world" });
+}
