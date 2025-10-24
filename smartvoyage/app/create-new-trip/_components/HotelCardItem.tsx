c8818ff54a022401d7767157aaf36c2d98353e3f
memoralesmapua <memorales@mymail.mapua.edu.ph>
2025-10-24 13:25:03 +0800

    HotelCardItem Component Update

---
+++ smartvoyage/app/create-new-trip/_components/HotelCardItem.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Image from "next/image";
+
+type Props ={
+  hotel: Hotel
+}
+function HotelCardItem({hotel}:Props) {
+  return (
+    <div className='flex flex-col gap-1'>
+                      <Image src={'/placeholder.jpg'} alt='place-image' width={400} height={200} 
+                      className='rounded-xl shadow object-cover mb-2'
+                      />
+                      <h2 className='font-semibold text-lg'>{hotel?.hotel_name}</h2>
+                      <h2 className='text-gray-500'>{hotel?.hotel_address}</h2>
+    
+                      <div className='flex justify-between items-center'>
+                      <p className='flex-gap-2 text-green-600'> <Wallet/>{hotel.price_per_night}</p>
+                      <p className='text-yellow-500 flex gap-2'><Star/>{hotel.rating}</p>
+                      </div>
+                      <Link href={'https://www.google.com/maps/search/?api=1&query='+hotel?.hotel_name} target='_blank'>
+                      <Button variant={'outline'} className='mt-1 w-full'>View</Button>
+                      </Link>
+                      {/*<p className='line-clamp-2 text-gray-500'>{hotel.description}</p>*/}
+                  </div>
+  )
+}
+import { Hotel } from './ChatBox'
+import { Link, Star, Wallet } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+
+export default HotelCardItem
--- smartvoyage/app/create-new-trip/_components/Itinerary.tsx
+++ smartvoyage/app/create-new-trip/_components/Itinerary.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Timeline } from "@/components/ui/timeline";
 import Image from "next/image";
-import { Clock, ExternalLink, Star, Ticket, Timer, Wallet } from 'lucide-react';
+import { Clock, ExternalLink, Link, Star, Ticket, Timer, Wallet } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import HotelCardItem from './HotelCardItem';
 
 
 const TRIP_DATA ={
@@ -104,6 +105,195 @@ const TRIP_DATA ={
 
             "best_time_to_visit": "string"
 
+          },
+          {
+
+            "place_name": "string",
+
+            "place_details": "string",
+
+            "place_image_url": "string",
+
+            "geo_coordinates": {
+
+              "latitude": "number",
+
+              "longitude": "number"
+
+            },
+
+            "place_address": "string",
+
+            "ticket_pricing": "string",
+
+            "time_travel_each_location": "string",
+
+            "best_time_to_visit": "string"
+
+          },
+          {
+
+            "place_name": "string",
+
+            "place_details": "string",
+
+            "place_image_url": "string",
+
+            "geo_coordinates": {
+
+              "latitude": "number",
+
+              "longitude": "number"
+
+            },
+
+            "place_address": "string",
+
+            "ticket_pricing": "string",
+
+            "time_travel_each_location": "string",
+
+            "best_time_to_visit": "string"
+
+          },
+          {
+
+            "place_name": "string",
+
+            "place_details": "string",
+
+            "place_image_url": "string",
+
+            "geo_coordinates": {
+
+              "latitude": "number",
+
+              "longitude": "number"
+
+            },
+
+            "place_address": "string",
+
+            "ticket_pricing": "string",
+
+            "time_travel_each_location": "string",
+
+            "best_time_to_visit": "string"
+
+          }
+
+        ]
+
+      },
+      {
+
+        "day": 2,
+
+        "day_plan": "string",
+
+        "best_time_to_visit_day": "string",
+
+        "activities": [
+
+          {
+
+            "place_name": "string",
+
+            "place_details": "string",
+
+            "place_image_url": "string",
+
+            "geo_coordinates": {
+
+              "latitude": "number",
+
+              "longitude": "number"
+
+            },
+
+            "place_address": "string",
+
+            "ticket_pricing": "string",
+
+            "time_travel_each_location": "string",
+
+            "best_time_to_visit": "string"
+
+          },
+          {
+
+            "place_name": "string",
+
+            "place_details": "string",
+
+            "place_image_url": "string",
+
+            "geo_coordinates": {
+
+              "latitude": "number",
+
+              "longitude": "number"
+
+            },
+
+            "place_address": "string",
+
+            "ticket_pricing": "string",
+
+            "time_travel_each_location": "string",
+
+            "best_time_to_visit": "string"
+
+          },
+          {
+
+            "place_name": "string",
+
+            "place_details": "string",
+
+            "place_image_url": "string",
+
+            "geo_coordinates": {
+
+              "latitude": "number",
+
+              "longitude": "number"
+
+            },
+
+            "place_address": "string",
+
+            "ticket_pricing": "string",
+
+            "time_travel_each_location": "string",
+
+            "best_time_to_visit": "string"
+
+          },
+          {
+
+            "place_name": "string",
+
+            "place_details": "string",
+
+            "place_image_url": "string",
+
+            "geo_coordinates": {
+
+              "latitude": "number",
+
+              "longitude": "number"
+
+            },
+
+            "place_address": "string",
+
+            "ticket_pricing": "string",
+
+            "time_travel_each_location": "string",
+
+            "best_time_to_visit": "string"
+
           }
 
         ]
@@ -124,20 +314,7 @@ function Itinerary() {
       content: (
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
           {TRIP_DATA?.hotels.map((hotel, index) => (
-              <div key={index} className='flex flex-col gap-1'>
-                  <Image src={'/placeholder.jpg'} alt='place-image' width={400} height={200} 
-                  className='rounded-xl shadow object-cover mb-2'
-                  />
-                  <h2 className='font-semibold text-lg'>{hotel?.hotel_name}</h2>
-                  <h2 className='text-gray-500'>{hotel?.hotel_address}</h2>
-
-                  <div className='flex justify-between items-center'>
-                  <p className='flex-gap-2 text-green-600'> <Wallet/>{hotel.price_per_night}</p>
-                  <p className='text-yellow-500 flex gap-2'><Star/>{hotel.rating}</p>
-                  </div>
-                  <Button variant={'outline'} className='mt-1'>View</Button>
-                  {/*<p className='line-clamp-2 text-gray-500'>{hotel.description}</p>*/}
-              </div>
+              <HotelCardItem hotel={hotel}/>
             ))}
         </div>
       ),
@@ -157,8 +334,9 @@ function Itinerary() {
                     <p className='text-gray-500 line-clamp-2'>{activity?.place_details}</p>
                     <h2 className='flex gap-2 text-blue-500 line-clamp-1'> <Ticket/>{activity?.ticket_pricing}</h2>
                     <p className='flex text-orange-400 gap-2 line-clamp-1'><Clock/>{activity?.best_time_to_visit}</p>
+                    <Link href={'https://www.google.com/maps/search/?api=1&query='+activity?.place_name} target='_blank'>
                     <Button size={'sm'} variant={'outline'} className='w-full mt-2'>View <ExternalLink/></Button>
-                    
+                    </Link>
                 </div>
             ))}
             </div>
